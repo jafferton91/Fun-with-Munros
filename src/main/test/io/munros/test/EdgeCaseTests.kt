51fc10700e4d84eff3f7ee3189b33198ef8f89c6
Jesson Atherton <jafferton91@gmail.com>
2018-12-12 18:06:24 +0000

    adding tests mentioned in doc

--- src/main/test/io/munros/test/EdgeCaseTests.kt
+++ src/main/test/io/munros/test/EdgeCaseTests.kt
@@ -4,9 +4,11 @@ import io.munros.library.data.enums.MunroCategory
 import io.munros.library.filterHeights
 import io.munros.library.makeNice
 import io.munros.library.util.CsvData
+import io.munros.library.util.MunroException
 import io.munros.test.helper.createCsvDataForTest
 import org.junit.Test
 import kotlin.test.assertEquals
+import kotlin.test.assertFailsWith
 
 internal class EdgeCaseTests : BaseTest() {
 
@@ -25,6 +27,21 @@ internal class EdgeCaseTests : BaseTest() {
         val listFilteredHeights = testSet.filterHeights(small, big)?.makeNice()?.take(numberToTake)
 
         assertEquals(numberToTake, listFilteredHeights?.size)
+
+    }
+
+    @Test fun filterHeightsAndMinHeightExceedsMaxHeight() {
+        val testSet = ArrayList<CsvData>()
+
+        testSet.add(createCsvDataForTest(benNevis, medium, MunroCategory.Top))
+        testSet.add(createCsvDataForTest(funNevis, smaller, MunroCategory.Top))
+        testSet.add(createCsvDataForTest(amyNevis, biggest, MunroCategory.Munro))
+        testSet.add(createCsvDataForTest(elfNevis, small, MunroCategory.Munro))
+        testSet.add(createCsvDataForTest(chrisNevis, big, MunroCategory.Munro))
+        testSet.add(createCsvDataForTest(daveNevis, smallest, MunroCategory.Top))
+
+        // Min height is larger than max height
+        assertFailsWith(MunroException::class) { testSet.filterHeights(big, small)?.makeNice() }
 
     }
 
